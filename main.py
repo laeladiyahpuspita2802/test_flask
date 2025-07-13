4a20fe0ad6e7313d79f330ed098b2354343011b5
laeladiyahpuspita2802 <laeladiyahpuspita280203@gmail.com>
2025-07-14 02:07:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # Ambil dari Railway
+    port = int(os.environ.get("PORT", 5000))  # Ambil dari Railway
     app.run(debug=True, host='0.0.0.0', port=port)
